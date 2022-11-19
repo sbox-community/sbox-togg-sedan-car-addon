8f6fe53fd53868791d1570ac7784fae7c8ef541f
byplt <elegayye@gmail.com>
2022-11-19 13:41:30 +0300

    Fix for Sandbox Gamemode

--- code/togg_sedan_car/CarCamera.cs
+++ code/togg_sedan_car/CarCamera.cs
@@ -1,166 +1,167 @@
 ﻿using Sandbox;
 using System;
-namespace Sandbox;
 
-public class ToggCamera : CameraMode
+namespace sbox.Community
 {
-	protected virtual float MinFov => 80.0f;
-	protected virtual float MaxFov => 100.0f;
-	protected virtual float MaxFovSpeed => 1000.0f;
-	protected virtual float FovSmoothingSpeed => 4.0f;
-	protected virtual float OrbitCooldown => 0.6f;
-	protected virtual float OrbitSmoothingSpeed => 25.0f;
-	protected virtual float OrbitReturnSmoothingSpeed => 4.0f;
-	protected virtual float MinOrbitPitch => -25.0f;
-	protected virtual float MaxOrbitPitch => 70.0f;
-	protected virtual float FixedOrbitPitch => 10.0f;
-	protected virtual float OrbitHeight => 35.0f;
-	protected virtual float OrbitDistance => 130.0f;
-	protected virtual float MaxOrbitReturnSpeed => 100.0f;
-	protected virtual float MinCarPitch => -60.0f;
-	protected virtual float MaxCarPitch => 60.0f;
-	protected virtual float CarPitchSmoothingSpeed => 1.0f;
-	protected virtual float CollisionRadius => 8.0f;
-	protected virtual float ShakeSpeed => 200.0f;
-	protected virtual float ShakeSpeedThreshold => 1000.0f;
-	protected virtual float ShakeMaxSpeed => 2500.0f;
-	protected virtual float ShakeMaxLength => 2.0f;
-
-	public bool orbitEnabled;
-	public TimeSince timeSinceOrbit;
-	public Angles orbitAngles;
-	public Rotation orbitYawRot;
-	public Rotation orbitPitchRot;
-	public float currentFov;
-	public float carPitch;
-	public Vector3 carPosition;
-
-	public override void Activated()
+	public class ToggCamera : CameraMode
 	{
-		orbitEnabled = false;
-		timeSinceOrbit = 0.0f;
-		orbitAngles = Angles.Zero;
-		orbitYawRot = Rotation.Identity;
-		orbitPitchRot = Rotation.Identity;
-		currentFov = MinFov;
-		carPitch = 0;
+		protected virtual float MinFov => 80.0f;
+		protected virtual float MaxFov => 100.0f;
+		protected virtual float MaxFovSpeed => 1000.0f;
+		protected virtual float FovSmoothingSpeed => 4.0f;
+		protected virtual float OrbitCooldown => 0.6f;
+		protected virtual float OrbitSmoothingSpeed => 25.0f;
+		protected virtual float OrbitReturnSmoothingSpeed => 4.0f;
+		protected virtual float MinOrbitPitch => -25.0f;
+		protected virtual float MaxOrbitPitch => 70.0f;
+		protected virtual float FixedOrbitPitch => 10.0f;
+		protected virtual float OrbitHeight => 35.0f;
+		protected virtual float OrbitDistance => 130.0f;
+		protected virtual float MaxOrbitReturnSpeed => 100.0f;
+		protected virtual float MinCarPitch => -60.0f;
+		protected virtual float MaxCarPitch => 60.0f;
+		protected virtual float CarPitchSmoothingSpeed => 1.0f;
+		protected virtual float CollisionRadius => 8.0f;
+		protected virtual float ShakeSpeed => 200.0f;
+		protected virtual float ShakeSpeedThreshold => 1000.0f;
+		protected virtual float ShakeMaxSpeed => 2500.0f;
+		protected virtual float ShakeMaxLength => 2.0f;
 
-		orbitYawRot = Rotation.FromYaw( Entity.Rotation.Yaw() );
-		orbitPitchRot = Rotation.Identity;
-		orbitAngles = (orbitYawRot * orbitPitchRot).Angles();
-	}
+		public bool orbitEnabled;
+		public TimeSince timeSinceOrbit;
+		public Angles orbitAngles;
+		public Rotation orbitYawRot;
+		public Rotation orbitPitchRot;
+		public float currentFov;
+		public float carPitch;
+		public Vector3 carPosition;
 
-	public override void Update()
-	{
-		var car = Entity as ToggSedan;
-		if ( !car.IsValid() ) return;
-
-		var body = car.PhysicsBody;
-		if ( !body.IsValid() ) return;
-
-		var speed = car.MovementSpeed;
-		var speedAbs = Math.Abs( speed );
-
-		if ( orbitEnabled && timeSinceOrbit > OrbitCooldown )
+		public override void Activated()
+		{
 			orbitEnabled = false;
+			timeSinceOrbit = 0.0f;
+			orbitAngles = Angles.Zero;
+			orbitYawRot = Rotation.Identity;
+			orbitPitchRot = Rotation.Identity;
+			currentFov = MinFov;
+			carPitch = 0;
 
-		var carRot = car.Rotation;
-		carPitch = carPitch.LerpTo( car.Grounded ? carRot.Pitch().Clamp( MinCarPitch, MaxCarPitch ) * (speed < 0.0f ? -1.0f : 1.0f) : 0.0f, Time.Delta * CarPitchSmoothingSpeed );
-
-		if ( orbitEnabled )
-		{
-			var slerpAmount = Time.Delta * OrbitSmoothingSpeed;
-
-			orbitYawRot = Rotation.Slerp( orbitYawRot, Rotation.From( 0.0f, orbitAngles.yaw, 0.0f ), slerpAmount );
-			orbitPitchRot = Rotation.Slerp( orbitPitchRot, Rotation.From( orbitAngles.pitch + carPitch, 0.0f, 0.0f ), slerpAmount );
-		}
-		else
-		{
-			var targetPitch = FixedOrbitPitch.Clamp( MinOrbitPitch, MaxOrbitPitch );
-			var targetYaw = speed < 0.0f ? carRot.Yaw() + 180.0f : carRot.Yaw();
-			var slerpAmount = MaxOrbitReturnSpeed > 0.0f ? Time.Delta * (speedAbs / MaxOrbitReturnSpeed).Clamp( 0.0f, OrbitReturnSmoothingSpeed ) : 1.0f;
-
-			orbitYawRot = Rotation.Slerp( orbitYawRot, Rotation.FromYaw( targetYaw ), slerpAmount );
-			orbitPitchRot = Rotation.Slerp( orbitPitchRot, Rotation.FromPitch( targetPitch + carPitch ), slerpAmount );
-
-			orbitAngles.pitch = orbitPitchRot.Pitch();
-			orbitAngles.yaw = orbitYawRot.Yaw();
-			orbitAngles = orbitAngles.Normal;
+			orbitYawRot = Rotation.FromYaw( Entity.Rotation.Yaw() );
+			orbitPitchRot = Rotation.Identity;
+			orbitAngles = (orbitYawRot * orbitPitchRot).Angles();
 		}
 
-		DoThirdPerson( car, body );
-
-		currentFov = MaxFovSpeed > 0.0f ? currentFov.LerpTo( MinFov.LerpTo( MaxFov, speedAbs / MaxFovSpeed ), Time.Delta * FovSmoothingSpeed ) : MaxFov;
-		FieldOfView = currentFov;
-
-		ApplyShake( speedAbs );
-	}
-
-	private void DoThirdPerson( ToggSedan car, PhysicsBody body )
-	{
-		Rotation = orbitYawRot * orbitPitchRot;
-
-		var carPos = car.Position + car.Rotation * (body.LocalMassCenter * car.Scale);
-		var startPos = carPos;
-		var targetPos = startPos + Rotation.Backward * (OrbitDistance * car.Scale* 2) + (Vector3.Up * (OrbitHeight * car.Scale));
-
-		var tr = Trace.Ray( startPos, targetPos )
-			.Ignore( car )
-			.Radius( Math.Clamp( CollisionRadius * car.Scale, 2.0f, 10.0f ) )
-			.WorldOnly()
-			.Run();
-
-		carPosition = tr.EndPosition;
-
-		Viewer = null;
-	}
-
-	/*public override void BuildInput( InputBuilder input )
-	{
-		base.BuildInput( input );
-
-		var pawn = Local.Pawn;
-		if ( pawn == null ) return;
-
-		if ( (Math.Abs( input.AnalogLook.pitch ) + Math.Abs( input.AnalogLook.yaw )) > 0.0f )
+		public override void Update()
 		{
-			if ( !orbitEnabled )
+			var car = Entity as ToggSedan;
+			if ( !car.IsValid() ) return;
+
+			var body = car.PhysicsBody;
+			if ( !body.IsValid() ) return;
+
+			var speed = car.MovementSpeed;
+			var speedAbs = Math.Abs( speed );
+
+			if ( orbitEnabled && timeSinceOrbit > OrbitCooldown )
+				orbitEnabled = false;
+
+			var carRot = car.Rotation;
+			carPitch = carPitch.LerpTo( car.Grounded ? carRot.Pitch().Clamp( MinCarPitch, MaxCarPitch ) * (speed < 0.0f ? -1.0f : 1.0f) : 0.0f, Time.Delta * CarPitchSmoothingSpeed );
+
+			if ( orbitEnabled )
 			{
-				orbitAngles = (orbitYawRot * orbitPitchRot).Angles();
-				orbitAngles = orbitAngles.Normal;
+				var slerpAmount = Time.Delta * OrbitSmoothingSpeed;
 
-				orbitYawRot = Rotation.From( 0.0f, orbitAngles.yaw, 0.0f );
-				orbitPitchRot = Rotation.From( orbitAngles.pitch, 0.0f, 0.0f );
+				orbitYawRot = Rotation.Slerp( orbitYawRot, Rotation.From( 0.0f, orbitAngles.yaw, 0.0f ), slerpAmount );
+				orbitPitchRot = Rotation.Slerp( orbitPitchRot, Rotation.From( orbitAngles.pitch + carPitch, 0.0f, 0.0f ), slerpAmount );
+			}
+			else
+			{
+				var targetPitch = FixedOrbitPitch.Clamp( MinOrbitPitch, MaxOrbitPitch );
+				var targetYaw = speed < 0.0f ? carRot.Yaw() + 180.0f : carRot.Yaw();
+				var slerpAmount = MaxOrbitReturnSpeed > 0.0f ? Time.Delta * (speedAbs / MaxOrbitReturnSpeed).Clamp( 0.0f, OrbitReturnSmoothingSpeed ) : 1.0f;
+
+				orbitYawRot = Rotation.Slerp( orbitYawRot, Rotation.FromYaw( targetYaw ), slerpAmount );
+				orbitPitchRot = Rotation.Slerp( orbitPitchRot, Rotation.FromPitch( targetPitch + carPitch ), slerpAmount );
+
+				orbitAngles.pitch = orbitPitchRot.Pitch();
+				orbitAngles.yaw = orbitYawRot.Yaw();
+				orbitAngles = orbitAngles.Normal;
 			}
 
-			orbitEnabled = true;
-			timeSinceOrbit = 0.0f;
+			DoThirdPerson( car, body );
 
-			orbitAngles.yaw += input.AnalogLook.yaw;
-			orbitAngles.pitch += input.AnalogLook.pitch;
-			orbitAngles = orbitAngles.Normal;
-			orbitAngles.pitch = orbitAngles.pitch.Clamp( MinOrbitPitch, MaxOrbitPitch );
+			currentFov = MaxFovSpeed > 0.0f ? currentFov.LerpTo( MinFov.LerpTo( MaxFov, speedAbs / MaxFovSpeed ), Time.Delta * FovSmoothingSpeed ) : MaxFov;
+			FieldOfView = currentFov;
+
+			ApplyShake( speedAbs );
 		}
 
-		input.ViewAngles = orbitEnabled ? orbitAngles : Entity.Rotation.Angles();
-		input.ViewAngles = input.ViewAngles.Normal;
-	}*/
+		private void DoThirdPerson( ToggSedan car, PhysicsBody body )
+		{
+			Rotation = orbitYawRot * orbitPitchRot;
 
-	private void ApplyShake( float speed )
-	{
-		if ( speed < ShakeSpeedThreshold )
-			return;
+			var carPos = car.Position + car.Rotation * (body.LocalMassCenter * car.Scale);
+			var startPos = carPos;
+			var targetPos = startPos + Rotation.Backward * (OrbitDistance * car.Scale * 2) + (Vector3.Up * (OrbitHeight * car.Scale));
 
-		var pos = Time.Now * ShakeSpeed;
-		var length = (speed - ShakeSpeedThreshold) / (ShakeMaxSpeed - ShakeSpeedThreshold);
-		length = length.Clamp( 0, ShakeMaxLength );
+			var tr = Trace.Ray( startPos, targetPos )
+				.Ignore( car )
+				.Radius( Math.Clamp( CollisionRadius * car.Scale, 2.0f, 10.0f ) )
+				.WorldOnly()
+				.Run();
 
-		float x = (0.5f - Noise.Simplex( pos )) * 2.0f * length;
-		float y = (0.5f - Noise.Perlin( pos, 5.0f )) * 2.0f * length;
+			carPosition = tr.EndPosition;
 
-		Position += Rotation.Right * x + Rotation.Up * y;
-		Rotation *= Rotation.FromAxis( Vector3.Up, x );
-		Rotation *= Rotation.FromAxis( Vector3.Right, y );
+			Viewer = null;
+		}
+
+		/*public override void BuildInput( InputBuilder input )
+		{
+			base.BuildInput( input );
+
+			var pawn = Local.Pawn;
+			if ( pawn == null ) return;
+
+			if ( (Math.Abs( input.AnalogLook.pitch ) + Math.Abs( input.AnalogLook.yaw )) > 0.0f )
+			{
+				if ( !orbitEnabled )
+				{
+					orbitAngles = (orbitYawRot * orbitPitchRot).Angles();
+					orbitAngles = orbitAngles.Normal;
+
+					orbitYawRot = Rotation.From( 0.0f, orbitAngles.yaw, 0.0f );
+					orbitPitchRot = Rotation.From( orbitAngles.pitch, 0.0f, 0.0f );
+				}
+
+				orbitEnabled = true;
+				timeSinceOrbit = 0.0f;
+
+				orbitAngles.yaw += input.AnalogLook.yaw;
+				orbitAngles.pitch += input.AnalogLook.pitch;
+				orbitAngles = orbitAngles.Normal;
+				orbitAngles.pitch = orbitAngles.pitch.Clamp( MinOrbitPitch, MaxOrbitPitch );
+			}
+
+			input.ViewAngles = orbitEnabled ? orbitAngles : Entity.Rotation.Angles();
+			input.ViewAngles = input.ViewAngles.Normal;
+		}*/
+
+		private void ApplyShake( float speed )
+		{
+			if ( speed < ShakeSpeedThreshold )
+				return;
+
+			var pos = Time.Now * ShakeSpeed;
+			var length = (speed - ShakeSpeedThreshold) / (ShakeMaxSpeed - ShakeSpeedThreshold);
+			length = length.Clamp( 0, ShakeMaxLength );
+
+			float x = (0.5f - Noise.Simplex( pos )) * 2.0f * length;
+			float y = (0.5f - Noise.Perlin( pos, 5.0f )) * 2.0f * length;
+
+			Position += Rotation.Right * x + Rotation.Up * y;
+			Rotation *= Rotation.FromAxis( Vector3.Up, x );
+			Rotation *= Rotation.FromAxis( Vector3.Right, y );
+		}
 	}
 }
-
--- code/togg_sedan_car/CarWheel.cs
+++ code/togg_sedan_car/CarWheel.cs
@@ -1,76 +1,80 @@
 ﻿using Sandbox;
 using System;
 
-struct ToggWheel
+
+namespace sbox.Community
 {
-	private readonly ToggSedan parent;
-
-	private float _previousLength;
-	private float _currentLength;
-
-	public ToggWheel( ToggSedan parent )
+	struct ToggWheel
 	{
-		this.parent = parent;
-		_previousLength = 0;
-		_currentLength = 0;
-	}
+		private readonly ToggSedan parent;
 
-	public bool Raycast( float length, bool doPhysics, Vector3 offset, ref float wheel, float dt )
-	{
-		var position = parent.Position;
-		var rotation = parent.Rotation;
+		private float _previousLength;
+		private float _currentLength;
 
-		var wheelAttachPos = position + offset;
-		var wheelExtend = wheelAttachPos - rotation.Up * (length * parent.Scale);
-
-		var tr = Trace.Ray( wheelAttachPos, wheelExtend )
-			.Ignore( parent )
-			.Ignore( parent.Driver )
-			.WithAllTags( "solid" )
-			.Run();
-
-		wheel = length * tr.Fraction;
-		var wheelRadius = (14 * parent.Scale);
-
-		if ( !doPhysics && ToggSedan.debug_car )
+		public ToggWheel( ToggSedan parent )
 		{
-			var wheelPosition = tr.Hit ? tr.EndPosition : wheelExtend;
-			wheelPosition += rotation.Up * wheelRadius;
-
-			if ( tr.Hit )
-			{
-				DebugOverlay.Circle( wheelPosition, rotation * Rotation.FromYaw( 90 ), wheelRadius, Color.Red.WithAlpha( 0.5f ), 0, false );
-				DebugOverlay.Line( tr.StartPosition, tr.EndPosition, Color.Red, 0, false );
-			}
-			else
-			{
-				DebugOverlay.Circle( wheelPosition, rotation * Rotation.FromYaw( 90 ), wheelRadius, Color.Green.WithAlpha( 0.5f ), 0, false );
-				DebugOverlay.Line( wheelAttachPos, wheelExtend, Color.Green, 0, false );
-			}
+			this.parent = parent;
+			_previousLength = 0;
+			_currentLength = 0;
 		}
 
-		if ( !tr.Hit || !doPhysics )
+		public bool Raycast( float length, bool doPhysics, Vector3 offset, ref float wheel, float dt )
 		{
-			return tr.Hit;
+			var position = parent.Position;
+			var rotation = parent.Rotation;
+
+			var wheelAttachPos = position + offset;
+			var wheelExtend = wheelAttachPos - rotation.Up * (length * parent.Scale);
+
+			var tr = Trace.Ray( wheelAttachPos, wheelExtend )
+				.Ignore( parent )
+				.Ignore( parent.Driver )
+				.WithAllTags( "solid" )
+				.Run();
+
+			wheel = length * tr.Fraction;
+			var wheelRadius = (14 * parent.Scale);
+
+			if ( !doPhysics && ToggSedan.debug_car )
+			{
+				var wheelPosition = tr.Hit ? tr.EndPosition : wheelExtend;
+				wheelPosition += rotation.Up * wheelRadius;
+
+				if ( tr.Hit )
+				{
+					DebugOverlay.Circle( wheelPosition, rotation * Rotation.FromYaw( 90 ), wheelRadius, Color.Red.WithAlpha( 0.5f ), 0, false );
+					DebugOverlay.Line( tr.StartPosition, tr.EndPosition, Color.Red, 0, false );
+				}
+				else
+				{
+					DebugOverlay.Circle( wheelPosition, rotation * Rotation.FromYaw( 90 ), wheelRadius, Color.Green.WithAlpha( 0.5f ), 0, false );
+					DebugOverlay.Line( wheelAttachPos, wheelExtend, Color.Green, 0, false );
+				}
+			}
+
+			if ( !tr.Hit || !doPhysics )
+			{
+				return tr.Hit;
+			}
+
+			var body = parent.PhysicsBody.SelfOrParent;
+
+			_previousLength = _currentLength;
+			_currentLength = (length * parent.Scale) - tr.Distance;
+
+			var springVelocity = (_currentLength - _previousLength) / dt;
+			var springForce = body.Mass * 50.0f * _currentLength;
+			var damperForce = body.Mass * (1.5f + (1.0f - tr.Fraction) * 3.0f) * springVelocity;
+			var velocity = body.GetVelocityAtPoint( wheelAttachPos );
+			var speed = velocity.Length;
+			var speedDot = MathF.Abs( speed ) > 0.0f ? MathF.Abs( MathF.Min( Vector3.Dot( velocity, rotation.Up.Normal ) / speed, 0.0f ) ) : 0.0f;
+			var speedAlongNormal = speedDot * speed;
+			var correctionMultiplier = (1.0f - tr.Fraction) * (speedAlongNormal / 1000.0f);
+			var correctionForce = correctionMultiplier * 50.0f * speedAlongNormal / dt;
+
+			body.ApplyImpulseAt( wheelAttachPos, tr.Normal * (springForce + damperForce + correctionForce) * dt );
+
+			return true;
 		}
-
-		var body = parent.PhysicsBody.SelfOrParent;
-
-		_previousLength = _currentLength;
-		_currentLength = (length * parent.Scale) - tr.Distance;
-
-		var springVelocity = (_currentLength - _previousLength) / dt;
-		var springForce = body.Mass * 50.0f * _currentLength;
-		var damperForce = body.Mass * (1.5f + (1.0f - tr.Fraction) * 3.0f) * springVelocity;
-		var velocity = body.GetVelocityAtPoint( wheelAttachPos );
-		var speed = velocity.Length;
-		var speedDot = MathF.Abs( speed ) > 0.0f ? MathF.Abs( MathF.Min( Vector3.Dot( velocity, rotation.Up.Normal ) / speed, 0.0f ) ) : 0.0f;
-		var speedAlongNormal = speedDot * speed;
-		var correctionMultiplier = (1.0f - tr.Fraction) * (speedAlongNormal / 1000.0f);
-		var correctionForce = correctionMultiplier * 50.0f * speedAlongNormal / dt;
-
-		body.ApplyImpulseAt( wheelAttachPos, tr.Normal * (springForce + damperForce + correctionForce) * dt );
-
-		return true;
 	}
 }
